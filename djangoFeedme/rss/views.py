fd56140349df44c248bbc249f7929fb9248288bb
jia#ub1804 <jia@ub1804.com>
2021-04-21 09:16:05 +0800

    integrate database display to view template

--- djangoFeedme/rss/views.py
+++ djangoFeedme/rss/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Rss
 
 # Create your views here.
 
 def rss_list(request):
-    return render(request, 'rss/rss_list.html')
+    rss_list = Rss.objects.all().order_by('title')
+    return render(request, 
+                  'rss/rss_list.html',
+                  {'rss_list':rss_list}
+                  )
