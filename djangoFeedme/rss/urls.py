4da7ec9c75c9d56454bc10efe262e952848197fc
jia#ub1804 <jia@ub1804.com>
2021-04-29 09:10:23 +0800

    added named URL for different RSS Feed source

--- djangoFeedme/rss/urls.py
+++ djangoFeedme/rss/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import re_path
 from . import views
 
+app_name = 'rss'
+
 urlpatterns = [
-    re_path(r'^$', views.rss_list),
-    re_path(r'^article$', views.article_list),
+    re_path(r'^$', views.rss_list, name='list'),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.rss_detail, name='detail'),
+    re_path(r'^article$', views.article_list, name='article'),
 
 ]
--- djangoFeedme/rss/views.py
+++ djangoFeedme/rss/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Rss,Articles
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -8,6 +9,14 @@ def rss_list(request):
     return render(request, 
                   'rss/rss_list.html',
                   {'rss_list':rss_list}
+            )
+# https://docs.djangoproject.com/en/dev/ref/models/querysets/#regex
+def rss_detail(request, slug):
+    # TODO what if article_list is empty
+    article_list = Articles.objects.filter(feedurl__icontains=slug)
+    return render(request, 
+                  'rss/article_list.html',
+                  {'article_list':article_list}
             )
 
 def article_list(request):
