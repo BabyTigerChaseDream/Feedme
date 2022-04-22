ff8e23afa0c18af675d956b5e9d05b69d035f398
BabyTigerChaseDream <scarlett_guojia@hotmail.com>
2022-04-22 12:39:14 +0800

    1) src/infra/curljson.sh parsed latest artical in json using curl/graphql 2) add htmltext page parser in webparser.py

--- src/infra/newsparser.py
+++ src/infra/newsparser.py
@@ -1,35 +1,61 @@
 
 import webparser as wp
+import inspect
 
-#nytimes_url = 'https://www.nytimes.com/section/world'
-nytimes_url = 'https://www.nytimes.com/international/section/opinion'
+nytimes_url = 'https://www.nytimes.com/section/world'
+#nytimes_url = 'https://www.nytimes.com/international/section/opinion'
+#nytimes_url = 'https://www.nytimes.com/section/world/asia'
 
-class news():
+class news:
     def __init__(self, url=nytimes_url):
         self.bs = wp.getPage(url)
     
-    def article(self,selector=None):
-        # all elements has a href&h2 title embeded
-        artblock_list=[a for a in self.bs.select('a') if a.find('h2')]
+    def get_block(self,selector=None):
+        try:
+            # all elements has a href&h2 title embeded
+            self.artblock_list=[a for a in self.bs.select('a') if a.find('h2')]
+        except Exception as e:
+            print(inspect.stack()[0][3])
 
-        # artical title
-        title_list = [h.find('h2').text for h in artblock_list]
-        # (title, link) of each title
-        title_href = [(a.find('h2').text, a['href']) for a in artblock_list]
-        # paraph of all articles
-        paraph_list = [a.find('p').text for a in  artblock_list]
+    def get_titles(self,selector=None):
+        try:
+            # artical title
+            self.titles = [h.find('h2').text for h in self.artblock_list]
+        except Exception as e:
+            print(inspect.stack()[0][3])   
+        return self.titles
 
-        # get date of each artile
+    def get_hrefs(self,selector=None):
+        try:
+            # (title, link) of each title
+            self.hrefs = [(a.find('h2').text, a['href']) for a in self.artblock_list]
+        except Exception as e:
+            print(inspect.stack()[0][3])
+        return self.hrefs
+
+    def get_paraph(self,selector=None):        
+        try:
+            # paraph of all articles
+            self.paraph= [a.find('p').text for a in  self.artblock_list]
+        except Exception as e:
+            print(inspect.stack()[0][3])
+        return self.paraph
 
     def search(self):
         # search keywords in 'search' tab
         pass    
+
     def getRss(self):
         # https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/section/world/asia/rss.xml
-# get RSS of all articles
+        pass
 
-hreflist=bs.select('a')
-len(hreflist)
-for h in hreflist:
-    if h['href'] and '2022' in h['href']:
-        print(h['href'])
+if __name__ == '__main__':
+    t_news = news(url=nytimes_url)
+    t_news.get_block()
+    t_news.get_titles()
+    t_news.get_hrefs()
+    t_news.get_paraph()
+
+    print('news : hrefs')
+    for n in t_news.hrefs:
+        print(n) 
